90722b386851c078c482debca351df97fd0322dd
Harshal <harshal.a.pradhan@gmail.com>
2021-05-01 16:30:56 +0530

    Updated required files.

--- user/manage-billing/update.php
+++ user/manage-billing/update.php
@@ -1,11 +1,16 @@
 <?php
+require_once('../../vendor/autoload.php');
+require_once('../../admin/config/Config.php');
+require_once('../../admin/config/Sample.Config.php');
+
+use \Stripe\BillingPortal\Session;
 
 require_once('../../admin/config/Config.php');
 
 //fetch customer id from logged in status
 $customer_id = 'cus_I6qZ1xTVH9RlBe';
 
-$session = \Stripe\BillingPortal\Session::create([
+$session = Session::create([
     'customer' => $customer_id,
     'return_url' => $return_url,
 ]);
